8392511593a86003dd6d2b99aff18db3f2f5c43e
godfreyhobbs <godfreyhobbs@gmail.com>
2018-05-19 00:23:17 -0400

    cleaning up some of the react code. have very basic UI working need to add action and other things

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 import './App.css';
 import getWeb3 from './utils/getWeb3'
 //TODO: move contract build folder
 import Splitter from './build/Splitter.json'
+
+// CONSTANTS
+const carolAddr = '0x05d89562a2b978B06Ba91c1BE137B32BF7CA3310';
+const bobAddr = '0x896838b0fdf2Edd193dF1AE4C5569D9503Ed118A';
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      deposit:0,
+      deposit: 0,
       numDestinationAddrs: 0,
       destinationAddrs: [],
-      balance: 0,
+      balances: {},
       web3: null
     }
   }
@@ -48,7 +53,7 @@ class App extends Component {
 
     // Declaring this for later so we can chain functions on SimpleStorage.
     this.state.web3.version.getNetwork(console.log)
-    
+
     var splitterInstance
 
     const carolAddr = '0x05d89562a2b978B06Ba91c1BE137B32BF7CA3310';
@@ -56,86 +61,69 @@ class App extends Component {
     //
     this.state.web3.eth.getAccounts((error, accounts) => {
 
+      this.updateAddrsBalances(accounts);
 
-      this.state.web3.eth.getBalance(accounts[0],
-         function (error, result) {
-           if (!error) {
-             console.log(JSON.stringify(result))
-             this.setState({balance: result.toString()})
-             splitter.deployed().then((instance) => {
-               splitterInstance = instance
+      splitter.deployed().then((instance) => {
+        splitterInstance = instance
 
-               return splitterInstance.numDestinationAddrs();
-             }).then((result) => {
-               console.log(result)
-               this.setState({numDestinationAddrs :result.toString()});
+        return splitterInstance.numDestinationAddrs();
+      }).then((result) => {
+        console.log(result)
+        this.setState({numDestinationAddrs: result.toString()});
 
-               return splitterInstance.destinationAddrs(carolAddr);
-             }).then((result) => {
-
-               if(!result[0]) {
-                 return splitterInstance.registerDestinationAddr('Carol', {from: carolAddr});
-               } else {
-                 return splitterInstance.registerDestinationAddr('Bob', {from: bobAddr});
-               }
+        return splitterInstance.destinationAddrs(carolAddr);
+      }).then((result) => {
+        //TODO: massive clean up of this init code
+        if (!result[0]) {
+          return splitterInstance.registerDestinationAddr('Carol', {from: carolAddr});
+        } else {
+          return splitterInstance.registerDestinationAddr('Bob', {from: bobAddr});
+        }
 
 
+      }).then((result) => {
+        console.log(result)
 
-             }).then((result) => {
-                console.log(result)
+        // Get the value from the contract to prove it worked.
+        //   return splitterInstance.registerDestinationAddr('Bob', {from: '0x5d72eabd566f050294262cf0fee30ee08ec8b00f'})
+        // }).then((result) => {
+        //   console.log(result)
+        //   return splitterInstance.send(this.state.web3.toWei(1, "ether"))
+        // }).then((result) => {
+        console.log(result)
+      })
 
-               // Get the value from the contract to prove it worked.
-             //   return splitterInstance.registerDestinationAddr('Bob', {from: '0x5d72eabd566f050294262cf0fee30ee08ec8b00f'})
-             // }).then((result) => {
-             //   console.log(result)
-             //   return splitterInstance.send(this.state.web3.toWei(1, "ether"))
-             // }).then((result) => {
-               console.log(result)
-             })
-
-           }
-           else
-             console.error(error);
-         }.bind(this)
-      )
     })
-
-
-    // Get accounts.
-    // this.state.web3.eth.getAccounts((error, accounts) => {
-    //   simpleStorage.deployed().then((instance) => {
-    //     simpleStorageInstance = instance
-    //
-    //     // Stores a given value, 5 by default.
-    //     return simpleStorageInstance.set(5, {from: accounts[0]})
-    //   }).then((result) => {
-    //     // Get the value from the contract to prove it worked.
-    //     return simpleStorageInstance.get.call(accounts[0])
-    //   }).then((result) => {
-    //     // Update state with the result.
-    //     return this.setState({ storageValue: result.c[0] })
-    //   })
-    // })
   }
+
+  updateAddrsBalances = async (accounts) => {
+      await _.forEach([accounts[0], bobAddr, carolAddr], async (addr) => {
+
+        this.state.web3.eth.getBalance(addr, (err, bal) => {
+          let newBalances = this.state.balances;
+          newBalances[addr] = bal.toString()
+          this.setState({balances: newBalances})
+        })
+      })
+    }
 
   render() {
 
-    const listAddrs = this.state.destinationAddrs.map((item) =>
-       <li>{item}</li>
-    );
+    const listAddrs = _.map(this.state.balances,((val, key) =>
+       <li key={key}>{key} : {val} </li>
+    ))
 
     return (
        <div className="App">
          <header className="App-header">
            <h1 className="App-title">Welcome to Slitter</h1>
          </header>
-         <p className="App-intro">
+         <div width='50%' className="App-intro">
            Splitter:
-           <ul>{listAddrs}</ul>,
+           <ul>{listAddrs}</ul>
            <div>numDestinationAddrs is {this.state.numDestinationAddrs}</div>
-           <div>destinationAddrs 1 : {this.state.destinationAddrs[0]}</div>
-           <div>account balance is {this.state.balance}</div>
-         </p>
+           {/*<div>destinationAddrs 1 : {this.state.destinationAddrs[0]}</div>*/}
+         </div>
        </div>
     );
   }
